742091fa2262eace1af749c47393f24c5ccbf47a
leonardonunes3 <leonardo.nunes793@gmail.com>
2023-12-26 23:39:07 -0300

    upload avatar

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,18 +3,17 @@ const multer = require("multer");
 const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
+const UsersAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthentication = require("../middlewares/ensureAuthentication");
 
 const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthentication, usersController.update);
-usersRoutes.patch("/avatar", ensureAuthentication, upload.single("avatar"), (request, response) => {
-    console.log(request.file.filename);
-    return response.json();
-});
+usersRoutes.patch("/avatar", ensureAuthentication, upload.single("avatar"), usersAvatarController.update);
 
 module.exports = usersRoutes;
